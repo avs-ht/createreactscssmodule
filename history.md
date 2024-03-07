1018e07bda8b69c6ea74688f9a96b8d65d334880
Artyom Suvorov <artyom.suvorov.shigoto@gmail.com>
2024-03-07 22:04:09 +0300

    v0.0.2

--- history.md
+++ history.md
@@ -1,16 +1,16 @@
-
-# Create React Style Module
-This extension allows you to create modular styles for .tsx and .jsx files
-
-### Author - [@r0bch4k](https://t.me/r0bch4k) 
-
-## Features
+# v1.0
 - Added 2 buttons to explorer context menu for .tsx/.jsx files: 
     - Add SCSS module
     - Add CSS module
 - Config:
     - startOfStyle (default: "styles") - this setting is needed while browsing the file for styles, to then add them to the style file. Also this setting is needed for hotkeys 
 - Hotkeys:
+    - Ctrl/cmd + Q - Adds a construct such as "className={yourStartOfStyle.$0}" and moves the cursor to $0
+
+# v1.1
+- Hotkeys:
     - Ctrl/cmd + U - Add a construct such as "className={startOfStyle.$0}"
     - Ctrl/cmd + K - Add a construct such as "className={startOfStyle["$0"]}"
-    - $0 - cursor position
+- Fix:
+    - Removed repetition of the same names
+    - In addition to startOfStyle.className, the startOfStyle["className"] construct has been added
--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,10 @@ export function activate(context: vscode.ExtensionContext) {
     const newFileName = `${fileName}.module.${type}`;
     const newFilePath = path.resolve(dirName, newFileName); 
 
+    if (!prefix) {
+      vscode.window.showErrorMessage('startOfStyle пустой!');
+      return;
+    }
     if (fs.existsSync(newFilePath)) {
       vscode.window.showErrorMessage(`Файл ${newFileName} уже создан!`);
       return;
@@ -28,9 +32,20 @@ export function activate(context: vscode.ExtensionContext) {
         if (err) {throw err;}
       });
 
-      const regex = new RegExp(`${prefix}\\.\\w+`, 'g'); 
-      let matches = fileData.match(regex)?.toString().split(',').map(el => {
-        const className = el.substring(prefix!.length+1, el.length);
+      // regexp - /styles\.\w+|styles\["\w+"\]/
+      const regex = new RegExp(`${prefix}\\.\\w+|${prefix}\\["\\w+"\\]`, 'g'); 
+      const allMatches = fileData.match(regex)?.toString().split(',');
+      let matches = [...new Set(allMatches)]
+      .map(el => {
+        let className;
+        if (el[prefix.length] === '.') {
+          className = el.substring(prefix.length + 1, el.length);
+        } else if (el[prefix.length] === '[') {
+          const lastBracket = el.lastIndexOf(']');
+          className = el.substring(prefix.length + 2, lastBracket - 1);
+        } else {
+          className=`crsm: Спарсен непонятный класс: ${el}`;
+        }
         return (`.${className} {\n\n}`);
       });
       if (matches === undefined) {
@@ -45,9 +60,10 @@ export function activate(context: vscode.ExtensionContext) {
   };
   const addModuleSCSS = vscode.commands.registerCommand("crsm.addModuleSCSS", (uri : vscode.Uri) => addModule('scss', uri));
   const addModuleCSS = vscode.commands.registerCommand("crsm.addModuleCSS", (uri : vscode.Uri) => addModule('css', uri));
-  let pasteStyle = vscode.commands.registerCommand("crsm.pasteStyle", () => {
+
+  const pasteStyle = (postfix: '.' | '[""]') => () => {
     const prefix: string | undefined = vscode.workspace.getConfiguration("crsm").get('startOfStyle');
-    const textToInsert = `className={${prefix}.}`;
+    const textToInsert = `className={${prefix}${postfix}}`;
     const editor = vscode.window.activeTextEditor;
     if (!editor) {return;}
     const currPosition = editor.selection.active;
@@ -55,15 +71,20 @@ export function activate(context: vscode.ExtensionContext) {
     editor.edit(editBuilder => {
       editBuilder.insert(currPosition, textToInsert);
     }).then(() => {
-      const newPosition = editor.document.positionAt(editor.document.offsetAt(currPosition) + textToInsert.length - 1);
+      const newPosition = editor.document.positionAt(editor.document.offsetAt(currPosition) + textToInsert.length - (1 + (postfix === '[""]' ? 2 : 0)));
       const newSelection = new vscode.Selection(newPosition, newPosition);
       editor.selection = newSelection;
       editor.revealRange(newSelection);
     });
-  });
+  };
+  
+  const pasteStyleDot = vscode.commands.registerCommand("crsm.pasteStyleDot", pasteStyle('.'));
+  const pasteStyleBrackets = vscode.commands.registerCommand("crsm.pasteStyleBrackets", pasteStyle('[""]'));
+
   context.subscriptions.push(addModuleSCSS);
   context.subscriptions.push(addModuleCSS);
-  context.subscriptions.push(pasteStyle);
+  context.subscriptions.push(pasteStyleDot);
+  context.subscriptions.push(pasteStyleBrackets);
 }
 
 export function deactivate() {}
